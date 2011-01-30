36441c6624aef7604b1580e456940d3bdf20ab06
David <davgomgar@gmail.com>
2011-01-30 18:58:18 +0100

    Ejemplos de como hacer delayed interpolation Se implementan ejemplos para hacer interpolacion retardada haciendo que los valores de la cadena no los tomen en el momento, sino cuando nos convenga. Se han implementado de 2 formas: 1.- Usando Procs 2.- El string se genera contenido entre comillas simples. Se envuelve en comillas dobles y se llama a eval.

---
+++ strings/delayed_interpolation.rb
@@ -0,0 +1,18 @@
+#coding: utf-8
+=begin 
+  Ejemplos para mostrar como se puede retrasar la evaluacion en la interpolación de cadenas
+=end
+require './utils'
+include Util
+asteriscos
+texto_centrado "Existen 2 formas de retrasar la interpolación en cadenas."
+texto_centrado "1.- Uso de Procs"
+asteriscos
+coords = proc{|x,y,z| "Las coordenadas (x,y,z) son: (#{x},#{y},#{z})"}
+puts coords.call(9,6,3)
+asteriscos
+texto_centrado "2.- Crear cadena entre comillas simples y usar eval envolviendola con comillas dobles"
+asteriscos
+str = 'Me llamo #{nombre} y tengo la nacionalidad #{nacionalidad}'
+nombre,nacionalidad = "David","Española"
+puts eval('"' + str + '"')
